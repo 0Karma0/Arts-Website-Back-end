2a065591c432f1939840f5bc1bc694586a66425a
mahade <mahade.adib45@gmail.com>
2024-04-28 20:17:56 +0600

    vercel setup and deploy done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -11,7 +12,7 @@ app.use(express.json());
 //artsAndCrafts
 //eT8cj67aUU18Ufod
 
-const uri = "mongodb+srv://artsAndCrafts:eT8cj67aUU18Ufod@cluster0.7uejvxv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7uejvxv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
