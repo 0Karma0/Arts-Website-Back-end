ac869312c3a88d05aa2b91fd4ba7d13f2f924658
mahade <mahade.adib45@gmail.com>
2024-04-28 12:37:42 +0600

    singleProduct id fetched to use it to update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -37,6 +37,13 @@ async function run() {
       const result = await productCollection.find({ email: req.params.email }).toArray();
       res.send(result)
     })
+
+    app.get("/singleProduct/:id", async(req, res) => {
+      const result = await productCollection.findOne({_id: new ObjectId(req.params.id),});
+      res.send(result)
+    })
+
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
